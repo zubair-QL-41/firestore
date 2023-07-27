7780cab787a20cf7fbc9406a960dbf7e537c746c
Zubair Nazir <zubair.nazir@quokkalabs.com>
2023-07-27 14:32:55 +0530

    minor fix

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-import * as admin from "firebase-admin";
+import * as admin from 'firebase-admin';
 
-import path from "path";
+import path from 'path';
 
 //initialize firebase sdk with config json, in my case its firebase.json created in root folder of project
 
 admin.initializeApp({
   credential: admin.credential.cert(
-    path.resolve(__dirname, "./firebase.json")
+    path.resolve(__dirname, '../../firebase.json'),
   ),
 });
 
-console.log("done");
+console.log('done');
 
 export const getRecordById = async (collectionName: string, id: string) => {
   const docRef = await admin.firestore().collection(collectionName).doc(id);
